bf5e66d8bcdcfa4dd475a392ea9f4b15f51de1e7
oklekot <oklekot@griddynamics.com>
2024-05-04 19:52:12 +0200

    changes after review

--- src/main/java/com/griddynamics/gridhub/payment/util/ValidationUtil.java
+++ src/main/java/com/griddynamics/gridhub/payment/util/ValidationUtil.java
@@ -2,6 +2,7 @@ package com.griddynamics.gridhub.payment.util;
 
 import com.griddynamics.gridhub.payment.dto.CreditCardDto;
 import com.griddynamics.gridhub.payment.dto.PaypalDto;
+import org.apache.commons.lang3.StringUtils;
 
 public class ValidationUtil {
 
@@ -11,19 +12,19 @@ public class ValidationUtil {
   private final static String EMAIL_REGEX = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$";
   private final static String EXPIRATION_DATE_REGEX = "^(0[1-9]|1[0-2])/[0-9]{2}$";
   private boolean isValidCardNumber(String cardNumber) {
-    return cardNumber != null && cardNumber.matches(CARD_NUMBER_REGEX);
+    return !StringUtils.isEmpty(cardNumber) && cardNumber.matches(CARD_NUMBER_REGEX);
   }
 
   private boolean isValidCardHolder(String cardHolder) {
-    return cardHolder != null && cardHolder.matches(CARD_HOLDER_REGEX);
+    return !StringUtils.isEmpty(cardHolder) && cardHolder.matches(CARD_HOLDER_REGEX);
   }
 
   private boolean isValidExpirationDate(String expirationDate) {
-    return expirationDate != null && expirationDate.matches(EXPIRATION_DATE_REGEX);
+    return !StringUtils.isEmpty(expirationDate) && expirationDate.matches(EXPIRATION_DATE_REGEX);
   }
 
   private boolean isValidCvv(String cvv) {
-    return cvv != null && cvv.matches(CVV_REGEX);
+    return !StringUtils.isEmpty(cvv) && cvv.matches(CVV_REGEX);
   }
 
   public boolean validateCreditCard(CreditCardDto creditCardDto) {
@@ -34,7 +35,7 @@ public class ValidationUtil {
   }
 
   private boolean isValidEmail(String email) {
-    return email != null && email.matches(EMAIL_REGEX);
+    return !StringUtils.isEmpty(email) && email.matches(EMAIL_REGEX);
   }
 
   public boolean validatePaypal(PaypalDto paypalDto) {
